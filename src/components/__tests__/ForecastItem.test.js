359e525e1ae29e35ced150055df8fa80447bc7a6
frostiebot <chris@telestatic.net>
2016-12-09 12:33:18 -0500

    housekeeping update
    - npm dependencies now sorted alphabetically
    - eslintrc now includes "jest" and "node" envs
    - updates to some files based on failing linter tests

--- src/components/__tests__/ForecastItem.test.js
+++ src/components/__tests__/ForecastItem.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import ForecastItem from '../ForecastItem';
 
 it('renders and converts dates and kelvins properly', () => {
-    let forecast = { dt_txt: "2016-10-19 03:00:00", main: { temp: 303.15 }, weather: [ { id: 804 } ] };
+    let forecast = { dt_txt: '2016-10-19 03:00:00', main: { temp: 303.15 }, weather: [ { id: 804 } ] };
     const wrapper = shallow(<ForecastItem forecast={forecast} />);
 
     expect(wrapper.find('.forecast-date').first().text()).toBe('Oct 19th');
